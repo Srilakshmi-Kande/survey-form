569902f00811f0b864d3e1eb8fd12bcb44b53846
Srilakshmi-Kande <kandesrilakshmi764@gmail.com>
2026-03-08 19:46:35 +0530

    database update

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.listen(PORT, () => {
 })
 
 app.set("view engine",'ejs')
+app.set("views",path.join(__dirname,'views'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
